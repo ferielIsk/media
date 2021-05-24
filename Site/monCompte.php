6256108a6bdc6b2b9c84b5d821ada43d998baddc
Lilia Izri <lizri@a-139160.ups.u-psud.fr>
2021-05-24 22:33:11 +0200

    Correction form

--- Site/monCompte.php
+++ Site/monCompte.php
@@ -19,6 +19,7 @@
 				  		echo '<li><a href="monCompte.php">My account</a></li>';
 				  ?>
 				</ul>
+				<form class="barMenu" method="post" action="resultatsDeRecherche.php">
 				 <input type="text" name="recherche" placeholder="Search.."pattern="[A-Za-z0-9\s/]{1,20}" 
 			     title =" keyword must contain only letters and numbers ! no more than 20 characters"> </input> 
  			      
